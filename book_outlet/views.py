f731c6a6bb7445f9b770050a69cde7091287d17f
Elizaveta Kulinich <kulinich.eliz@gmail.com>
2022-12-05 21:58:05 +0300

    rendering the details page

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import Http404
 
 from .models import Book
 # Create your views here.
@@ -8,3 +9,13 @@ def index(request):
     return render(request, "book_outlet/index.html", {
         "books": books
     }) 
+
+
+def book_detail(request, id):
+    book = get_object_or_404(Book, pk=id)
+    return render(request, "book_outlet/book_detail.html",{
+        "title": book.title,
+        "author": book.author,
+        "rating": book.rating,
+        "is_bestselling": book.is_bestselling
+    })
